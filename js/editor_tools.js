aa4bab74f0c6843d1e03eef332cc1818dfed4f71
Marouandev <marouan.hammami@argolife.fr>
2016-09-30 17:57:47 +0100

    delete file tools.js

--- js/editor_tools.js
+++
@@ -1,6 +0,0 @@
-  
-  var cke = CKEDITOR.inline('target');
-  var btnClose = document.getElementById('btnClose');
-  btnClose.addEventListener('click', function(event){
-    cke.focusManager.blur(true);
-  });
